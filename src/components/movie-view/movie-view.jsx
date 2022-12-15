7ece81ec13a6c246834ad2fd384e4d5f2eb8d050
jdussold <jdussold@gmail.com>
2022-12-14 21:30:21 -0600

    destructured MovieView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,24 +1,26 @@
 export const MovieView = ({ movie, onBackClick }) => {
+  const { image, title, description, genre, director } = movie;
+
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img src={image} />
       </div>
       <div>
         <span>Title: </span>
-        <span>{movie.title}</span>
+        <span>{title}</span>
       </div>
       <div>
         <span>Description: </span>
-        <span>{movie.description}</span>
+        <span>{description}</span>
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movie.genre}</span>
+        <span>{genre}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie.director}</span>
+        <span>{director}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
