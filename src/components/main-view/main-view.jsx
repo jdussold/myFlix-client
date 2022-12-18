4077401f0d7faea5b05fb4b4fe343f70353a5aa0
jdussold <jdussold@gmail.com>
2022-12-18 12:11:56 -0600

    ensure "Loading..." shows up before "list is empty"

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -44,6 +44,10 @@ export const MainView = () => {
     );
   }
 
+  if (loading) {
+    return <LoadingIndicator />;
+  }
+
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
   }
