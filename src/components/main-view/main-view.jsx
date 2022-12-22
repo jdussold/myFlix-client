bf1936daeb3a0e077756aafeb900ca55718069c8
jdussold <jdussold@gmail.com>
2022-12-22 17:12:40 -0600

    Refactored with React Bootstrap rows and columns

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,25 +3,31 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export const MainView = () => {
+  // Declare state variables to hold movies, user and token data, and a loading indicator
   const [movies, setMovies] = useState([]);
   const storedUser = JSON.parse(localStorage.getItem("user"));
   const storedToken = localStorage.getItem("token");
   const [user, setUser] = useState(storedUser ? storedUser : null);
   const [token, setToken] = useState(storedToken ? storedToken : null);
   const [loading, setLoading] = useState(true);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
     if (!token) {
       return;
     }
 
+    // Fetch movie data when authorized
     fetch("https://my-flix-db-jd.herokuapp.com/movies", {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then((response) => response.json())
       .then((data) => {
+        // Map movie data from the API to a new format
         const moviesFromApi = data.map((movie) => {
           return {
             title: movie.Title,
@@ -41,78 +47,58 @@ export const MainView = () => {
       });
   }, [token]);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
-
+  // Render a loading indicator
   const LoadingIndicator = () => {
     return <div>Loading...</div>;
   };
 
-  if (!user) {
-    return (
-      <>
-        <LoginView
-          onLoggedIn={(user, token) => {
-            setUser(user);
-            setToken(token);
-          }}
-        />
-        or
-        <SignupView />
-      </>
-    );
-  }
-
-  if (selectedMovie) {
-    return (
-      <div>
-        <MovieView
-          movie={selectedMovie}
-          onBackClick={() => setSelectedMovie(null)}
-        />
-
-        <button
-          onClick={() => {
-            setUser(null);
-            setToken(null);
-            localStorage.clear();
-          }}
-        >
-          Logout
-        </button>
-      </div>
-    );
-  }
-
-  if (loading) {
-    return <LoadingIndicator />;
-  }
-
-  if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-  }
-
-  if (!loading) {
-    return (
-      <div>
-        {movies.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            movie={movie}
-            onMovieClick={(newSelectedMovie) => {
-              setSelectedMovie(newSelectedMovie);
-            }}
+  return (
+    <Row className="justify-content-md-center">
+      {!user ? (
+        // If the user is not logged in, render the LoginView and SignupView components
+        <Col md={5}>
+          <LoginView onLoggedIn={(user) => setUser(user)} />
+          or
+          <SignupView />
+        </Col>
+      ) : loading ? (
+        // If the user is logged in and the data is still loading, render the LoadingIndicator
+        <LoadingIndicator />
+      ) : movies.length === 0 ? (
+        // If the user is logged in, the data is finished loading, and the movies array is empty, render a message saying the list is empty
+        <div>The list is empty!</div>
+      ) : //If a movie has been selected, render the MovieView component
+      selectedMovie ? (
+        <Col md={8}>
+          <MovieView
+            movie={selectedMovie}
+            onBackClick={() => setSelectedMovie(null)}
           />
-        ))}
-        <button
-          onClick={() => {
-            setUser(null);
-            setToken(null);
-            localStorage.clear();
-          }}
-        >
-          Logout
-        </button>
-      </div>
-    );
-  }
+        </Col>
+      ) : (
+        //If none of the above conditions are met, render the movie cards and a logout button
+        <>
+          {movies.map((movie) => (
+            <Col className="mb-5" key={movie.id} md={3}>
+              <MovieCard
+                movie={movie}
+                onMovieClick={(newSelectedMovie) => {
+                  setSelectedMovie(newSelectedMovie);
+                }}
+              />
+            </Col>
+          ))}
+          <button
+            onClick={() => {
+              setUser(null);
+              setToken(null);
+              localStorage.clear();
+            }}
+          >
+            Logout
+          </button>
+        </>
+      )}
+    </Row>
+  );
 };
