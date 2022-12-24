840e5a2ab61a64f09ce5ef48214eb8a306ef0b5b
jdussold <jdussold@gmail.com>
2022-12-23 22:53:45 -0600

    Added multiple comments
    added additional comments to help better explain what is happening with each part of the code.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,29 +1,38 @@
-import "./movie-view.scss";
+import "./movie-view.scss"; // Import the styles for the component
 
+// Define the MovieView component
 export const MovieView = ({ movie, onBackClick }) => {
+  // Destructure the movie object to get the image, title, description, genre, and director
   const { image, title, description, genre, director } = movie;
 
+  // Return the JSX that will be rendered for this component
   return (
     <div>
+      {/* Render the movie image */}
       <div>
         <img src={image} className="img-fluid w-50 d-block mx-auto" />
       </div>
+      {/* Render the title */}
       <div>
         <span>Title: </span>
         <span>{title}</span>
       </div>
+      {/* Render the description */}
       <div>
         <span>Description: </span>
         <span>{description}</span>
       </div>
+      {/* Render the genre */}
       <div>
         <span>Genre: </span>
         <span>{genre}</span>
       </div>
+      {/* Render the director */}
       <div>
         <span>Director: </span>
         <span>{director}</span>
       </div>
+      {/* Render the back button with a click event handler */}
       <button
         onClick={onBackClick}
         className="back-button"
