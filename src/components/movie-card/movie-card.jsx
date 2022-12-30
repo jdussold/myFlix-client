76450d5d22549c6aa0a3adecdc012e5919d38d94
jdussold <jdussold@gmail.com>
2022-12-30 00:06:43 -0600

    removed onClick events

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,14 @@
+import React from "react";
 //Import the PropTypes library
 import PropTypes from "prop-types";
-
 // Import the Button and Card components from the react-bootstrap library
 import { Button, Card } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
 //The MovieCard Function Component
 // The component returns a Card element with an image, title, director, and a button
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
-    <Card className="h-100" onClick={() => onMovieClick(movie)}>
+    <Card className="h-100">
       <Card.Img
         variant="top"
         src={movie.image}
@@ -18,10 +18,18 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       <Card.Body className="text-center">
         <Card.Title className="text-center">{movie.title}</Card.Title>
         <Card.Text>{movie.director}</Card.Text>
-        <Button onClick={() => onMovieClick(movie)} className="btn">
-          Details
-        </Button>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button className="btn">Details</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    director: PropTypes.string,
+  }).isRequired,
 };
