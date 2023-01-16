b94b61f13d271e5ba2cd01b7f71446fc890ebae2
jdussold <jdussold@gmail.com>
2023-01-15 19:21:46 -0600

    added searching and filtering
    Also updated with comments

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,14 +1,28 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./navigation-bar.scss";
 
-export const NavigationBar = ({ user, onLoggedOut }) => {
+export const NavigationBar = ({
+  user,
+  onLoggedOut,
+  handleSearchInput,
+  handleFilterSelection,
+}) => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
+        {/* Setting the brand name to MyFlix, with a link to the home page */}
         <Navbar.Brand style={{ color: "#FFA400" }} as={Link} to="/">
           MyFlix
         </Navbar.Brand>
+        {/* Toggling navigation bar for smaller screens */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -38,6 +52,32 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
               </>
             )}
           </Nav>
+          {/* If a user is logged in then display search bar */}
+          {user && (
+            <Form inline className="d-flex">
+              {/*Input field for searching */}
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2 mx-2 orange-border"
+                onChange={handleSearchInput}
+              />
+              {/* Dropdown for filter selection*/}
+              <FormControl
+                as="select"
+                onChange={handleFilterSelection}
+                className="mx-2 orange-border"
+              >
+                <option value="title">Title</option>
+                <option value="genre">Genre</option>
+                <option value="director">Director</option>
+              </FormControl>
+              {/* Search button*/}
+              <Button variant="outline-success" className="orange-outline-btn">
+                Search
+              </Button>
+            </Form>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
