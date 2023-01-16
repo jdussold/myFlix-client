0730a2a15d088360b21b3336633047a5d8b86b9e
jdussold <jdussold@gmail.com>
2023-01-15 19:21:11 -0600

    Updated with comments

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,13 +4,16 @@ import { Button } from "react-bootstrap";
 import "./movie-view.scss";
 
 export const MovieView = ({ movies }) => {
+  // Get the movieId from the URL parameters
   const { movieId } = useParams();
 
+  // Find the movie in the movies array that matches the movieId
   const movie = movies.find((m) => m.id === movieId);
 
   return (
     <div className="center-div">
       <div>
+        {/* Display the movie's poster */}
         <img
           className="img-poster my-4 img-fluid w-50 d-block mx-auto"
           src={movie.image}
@@ -18,31 +21,36 @@ export const MovieView = ({ movies }) => {
       </div>
       <div>
         <span>
+          {/* Display the movie's title */}
           <strong>Title:</strong>{" "}
         </span>
         <span>{movie.title}</span>
       </div>
       <div>
         <span>
+          {/* Display the movie's director */}
           <strong>Director:</strong>{" "}
         </span>
         <span>{movie.director}</span>
       </div>
       <div>
         <span>
+          {/* Display the movie's description */}
           <strong>Description:</strong>{" "}
         </span>
         <span>{movie.description}</span>
       </div>
       <div>
         <span>
+          {/* Display the movie's genre */}
           <strong>Genre:</strong>{" "}
         </span>
         <span>{movie.genre}</span>
       </div>
       <div className="mt-4 text-center">
+        {/* Link to the homepage */}
         <Link to={`/`}>
-          <Button className="btn my-2">Back</Button>
+          <Button className="btn my-2">Home</Button>
         </Link>
       </div>
     </div>
